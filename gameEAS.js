704c071c9fce8fe01ceea9f76bd3a1258e2867de
NetanelSonkin <NetanelSonkin@users.noreply.github.com>
2024-09-13 19:09:56 +0300

    Add start and reset button

--- gameEAS.js
+++ gameEAS.js
@@ -1,17 +1,37 @@
-const createDiv = () => {
-        let gridDiv = document.createElement('div');
-        gridDiv.className = 'newDiv';
 
-        return gridDiv;
+const startGame = () => {
+const createTile = () => {
+        let gridTile = document.createElement('div');
+        gridTile.className = 'newTile';
+
+        return gridTile;
 }
 
 const createGrid = () => {
     const container = document.querySelector('#container');
 
     for(let i = 0; i < 256; i++) {
-        const div = createDiv();
+        const div = createTile();
         container.appendChild(div);
     }
 }
 
-window.onload = createGrid;
+const colorTile = () => {
+}
+}
+
+const wrapper = document.querySelector("#wrapper");
+const btnList = document.createElement("ul");
+const startList = document.createElement("li");
+const resetList = document.createElement("li");
+const startBtn = document.createElement("button");
+const resetBtn = document.createElement("button");
+
+startBtn.setAttribute("class", "startGame");
+resetBtn.setAttribute("class", "resetGame");
+startBtn.textContent = "Start Game!";
+resetBtn.textContent = "Reset";
+startList.appendChild(startBtn);
+resetList.appendChild(resetBtn);
+btnList.append(startList,resetList);
+wrapper.insertBefore(btnList,container);
